52ea5290d245d2363f71498e4affd703a85a489c
Guohao0509 <277482458@qq.com>
2017-10-16 15:49:46 +0800

    Production Environment -V1.0 By Guohao

--- bus-wechat/public/spa/a-bugubus/controller3.js
+++ bus-wechat/public/spa/a-bugubus/controller3.js
@@ -271,8 +271,26 @@ app.controller('ScheduleDetailController',function($ionicPopup,$window,$rootScop
     /*
      * 每隔10s请求
      * */
-    var content="<div class='bus-image marker '></div>";
+    var content="<div class='bus-image marker'></div>";
     var marker;
+    $myHttpService.postNoLoad('api/busline/queryCarLocation',{carid:$scope.busSchedule.carid},function(data){
+        /*接收数据*/
+        // console.log(data)
+        //alert('lnglat:'+lnglat);
+        /*判断marker是否存在*/
+        if(!marker){
+            //创建一个地图对象
+            var marker = new AMap.Marker({
+                map: map,
+                position:[data.car.currlon,data.car.currlat],
+                content:content,
+                draggable:false
+            });
+        }else{
+            marker.setPosition($scope.lnglat.split(','));
+        }
+
+    })
     $interval(function(){
             $myHttpService.postNoLoad('api/busline/queryCarLocation',{carid:$scope.busSchedule.carid},function(data){
                 /*接收数据*/
