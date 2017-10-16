22ed827be8dc9226e74e049d051fcd35d2ef0b3f
Guohao0509 <277482458@qq.com>
2017-10-16 17:20:26 +0800

    Production Environment -V1.0 By Guohao

--- bus-wechat/public/spa/a-bugubus/controller2.js
+++ bus-wechat/public/spa/a-bugubus/controller2.js
@@ -1084,7 +1084,6 @@ var app = angular.module('app')
                 }else{
                     marker.setPosition($scope.lnglat.split(','));
                 }
-
             })
         },
         10000
